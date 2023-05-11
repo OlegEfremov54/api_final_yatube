858dedcdc09836853c32f4e18a4c263bbf04d21e
Îëåã Åôðåìîâ <ole_f@mail.ru>
2023-05-11 18:00:56 +0500

    Ф9 Замечания ревью

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import PostsViewSet, GroupsViewSet, CommentsViewSet, FollowsViewSet
+from .views import CommentsViewSet, FollowsViewSet, GroupsViewSet, PostsViewSet
 
 router_v1 = routers.DefaultRouter()
 router_v1.register(r'posts', PostsViewSet, basename='posts')
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,17 +1,13 @@
 # TODO:  Напишите свой вариант
+from django_filters.rest_framework import DjangoFilterBackend
 from posts.models import Group, Post
-from rest_framework import filters, permissions, viewsets, mixins
+from rest_framework import filters, mixins, permissions, viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
-from django_filters.rest_framework import DjangoFilterBackend
 
 from .permissions import IsAuthorOrReadOnly
-from .serializers import (
-    CommentSerializer,
-    FollowSerializer,
-    GroupSerializer,
-    PostSerializer
-)
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
 
 
 class PostsViewSet(viewsets.ModelViewSet):
